dac7617a0cbe278a205aa21f5c801e95f5a8bb66
PM2011 <abc5593449@hotmail.com>
2016-06-06 23:24:08 +0800

    使用公式計算答案

--- a216數數愛明明/main.c
+++ a216數數愛明明/main.c
@@ -6,48 +6,41 @@
 
 #include <stdio.h>
 
-#define MAX_G_DATA 30000
-
-typedef long long int lli;
-
-lli f(int n)
-{
-	/* 上底+下底*高/2 */
-	debug(
-		printf("[DEBUG] f(%d)=%lld\n",
-			n, ((1 + (lli)n) * n) >> 1)
-	);
-	return ((1 + n) * n) >> 1;
-}
-
-lli gData[MAX_G_DATA] = { 0 };
-
-lli g(int n)
-{
-	/* 可等價成g(n)=f(n)+f(n-1)+...+f(1) */
-
-	if (n <= MAX_G_DATA)
-	{
-		if (gData[n] == 0)
-		{
-			debug(
-				printf("[DEBUG] g(%d)=%lld\n",
-					n, gData[n])
-			);
-			return gData[n] = (n > 1) ? f(n) + g(n - 1) : n;
-		}
-		return gData[n];
-	}
-
-	return (n > 1) ? f(n) + g(n - 1) : n;
-}
-
 int main(int argc, char *argv[])
 {
 	int n;
 	while (scanf("%d", &n) != EOF)
 	{
-		printf("%lld %lld\n", f(n), g(n));
+		/*
+			f(n)=(n)+(n-1)+...+1, f(1)=1
+			f(n)=(n+1)*n/2
+
+			g(n)=f(n)+g(n-1), g(1)=1
+			=>g(n)=f(n)+f(n-1)+g(n-1)
+			=>g(n)=f(n)+f(n-1)+f(n-2)+...+f(2)+g(1)
+			=>g(n)=f(n)+f(n-1)+f(n-2)+...+f(2)+f(1)
+			=>		[(n+1)*n/2]
+					+[((n-1)+1)*(n-1)/2]
+					+[((n-2)+1)*(n-2)/2]
+					+...
+					+[(2+1)*(2)/2]
+					+[(1+1)*(1)/2]
+			=>	{	[(n+1)*n]
+					+[((n)*(n-1)]
+					+[((n-1)*(n-2)]
+					+...
+					+[(3)*(2)]
+					+[(2)*(1)]
+				}/2
+			=>g(n)={[n]sigma[x=1] ((x+1)*x)/2}
+			=>g(n)={[n]sigma[x=1] ((x+1)*x)}/2
+			=>g(n)={n*(n+1)*(n+2)/3}/2
+			=>g(n)=n*(n+1)*(n+2)/6
+		*/
+		printf("%lld %lld\n", 
+			(long long int)(n + 1) * n / 2,
+			(long long int)n * (n + 1) * (n + 2) / 6
+		);
 	}
 
 	return 0;
