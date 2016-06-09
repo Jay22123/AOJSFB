15b1bb62341d85bce0dbdc71c86a8f8b9687b7c2
PM2011 <abc5593449@hotmail.com>
2016-06-09 22:51:46 +0800

    a017五則運算-clear.

--- a017五則運算/main.cpp
+++ a017五則運算/main.cpp
@@ -5,13 +5,18 @@
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
 
 int getLine(char *strBuf)
 {
 	char input;
 
 	int isEOF = 0, seek = ftell(stdin);
-	while (true)
+	while (1)
 	{
 		isEOF = fscanf(stdin, "%c", &input);
 		if (input == '\n')
@@ -32,13 +37,170 @@ int getLine(char *strBuf)
 	return isEOF;
 }
 
+
+/* 堆疊相關程式 開始 */
+typedef struct _Stack
+{
+	int data;
+	struct _Stack *prev;
+}Stack;
+
+void push(Stack **stack, int data)
+{
+	Stack *input = (Stack *)malloc(sizeof(Stack));
+	input->data = data;
+	input->prev = *stack;
+	*stack = input;
+}
+
+int pop(Stack **stack)
+{
+	Stack *popStack = *stack;
+	*stack = popStack->prev;
+	int result = popStack->data;
+
+	popStack->prev = NULL;
+	free(popStack);
+	return result;
+}
+
+int last(Stack **stack)
+{
+	return (*stack)->data;
+}
+/* 堆疊相關程式 結束 */
+
+
+/* 後序式運算 */
+int postfixCal(char *input)
+{
+	Stack *numStack = NULL;
+
+	int i;
+	for (i = 0; i < strlen(input); i++)
+	{
+		if (isdigit(input[i]))
+		{
+			int num;
+			sscanf(&input[i], "%d", &num);
+			push(&numStack, num);
+
+			i += (int)log10(num) + 1;
+		}
+		else if (ispunct(input[i]))
+		{
+			int b = pop(&numStack),
+				a = pop(&numStack),
+				c = 0;
+
+			switch (input[i])
+			{
+			case '+': c = a + b; break;
+			case '-': c = a - b; break;
+			case '*': c = a * b; break;
+			case '/': c = a / b; break;
+			case '%': c = a % b; break;
+			}
+			push(&numStack, c);
+		}
+	}
+
+	return pop(&numStack);
+}
+
+
+// 運算式結尾定義
+enum
+{
+	EOE = 0
+};
+
+// 輸入優先級
+int icp(char op)
+{
+	switch (op)
+	{
+	case '+':
+	case '-': return 12;
+	case '*':
+	case '/':
+	case '%': return 13;
+	case '(': return 20;
+	case ')': return 19;
+	case EOE: return EOE;
+	}
+}
+
+// 堆疊優先級
+int isp(char op)
+{
+	switch (op)
+	{
+	case '+':
+	case '-': return 12;
+	case '*':
+	case '/':
+	case '%': return 13;
+	case '(': return 0;
+	case ')': return 19;
+	case EOE: return EOE;
+	}
+}
+
+
 int main(int argc, char *argv[])
 {
-	char str[1000] = { NULL };
+	char str[200] = { NULL };
 
 	while (getLine(str) != EOF)
 	{
-		printf("%s\n", str);
+		debug(printf("[DEBUG] %s\n", str));
+
+		char equation[200] = "";
+
+		Stack *opStack = NULL;
+		push(&opStack, EOE);
+		char *strPtr = str;
+		for (; *strPtr != '\0'; strPtr++)
+		{
+			if (*strPtr == ' ')
+				continue;
+			else if (isdigit(*strPtr))
+			{
+				int num = 0;
+				sscanf(strPtr, "%d", &num);
+				sprintf(equation, "%s %d", equation, num);
+				strPtr += (int)log10(num);
+			}
+			else if (ispunct(*strPtr))
+			{
+				if (*strPtr == ')')
+				{
+					while (last(&opStack) != '(')
+					{
+						sprintf(equation, "%s %c", equation, pop(&opStack));
+					}
+					pop(&opStack); // pop '('
+				}
+				else
+				{
+					while ((isp(last(&opStack)) >= icp(*strPtr)))
+					{
+						sprintf(equation, "%s %c", equation, pop(&opStack));
+					}
+					push(&opStack, *strPtr);
+				}
+			}
+		}
+
+		while (last(&opStack) != EOE)
+		{
+			sprintf(equation, "%s %c", equation, pop(&opStack));
+		}
+		pop(&opStack); // pop 'EOE'
+
+		debug(printf("[DEBUG] %s\n", equation));
+		printf("%d\n", postfixCal(equation));
 	}
 
 	return 0;
