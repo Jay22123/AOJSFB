4905463fe7f890e6a4601f100a4d13f929c72d26
PM2011 <abc5593449@hotmail.com>
2016-07-02 16:04:57 +0800

    抓取輸入資料

--- b816大家都進來吧/main.c
+++ b816大家都進來吧/main.c
@@ -5,14 +5,49 @@
 #endif
 
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
-	char s[1000] = { NULL };
+	int n = 0;
 
-	while (scanf("%s", &s) != EOF)
+	/*
+		i < j < k, a_i <= a_j <= a_k，並最大化(a_j - a_i) * (a_k - a_j)
+		保證存在一組i, j, k使得i < j < k且a_i <= a_j <= a_k。
+	*/
+
+	while (scanf("%d", &n) != EOF)
 	{
-		printf("hello, %s\n", s);
+		int i, a_n = -1, a_i = -1, a_j = -1, a_k = -1, maxNum = 0;
+		for (i = 0; i < n; i++)
+		{
+			scanf("%d", &a_n);
+
+			if (a_i < a_n)
+			{
+				a_i = (int)fmin(a_n, a_i);
+				a_k = (int)fmax(a_n, a_k);
+
+				if (a_n < a_k)
+					a_j = a_n;
+				else
+					a_j = -1;
+
+				maxNum = (int)fmax((a_j - a_i) * (a_k - a_j), maxNum);
+			}
+			else
+			{
+				a_i = 500000;
+				a_j = -1;
+				a_k = 0;
+			}
+		}
+		/*debug(
+			printf("[DEBUG] maxIndex=%d, minIndex=%d\n"
+				, maxIndex, minIndex)
+		);*/
+
+		printf("%d\n", maxNum);
 	}
 
 	return 0;
