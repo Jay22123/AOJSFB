72dda7df765f82de6759e598beb059ac877853df
PM2011 <abc5593449@hotmail.com>
2016-07-02 19:31:12 +0800

    b818拿數字-clear.

--- b818拿數字/main.c
+++ b818拿數字/main.c
@@ -6,13 +6,66 @@
 
 #include <stdio.h>
 
+int first, last, ai[10000] = { 0 };
+
+int getBigNum(int *sum)
+{
+	if (first >= 0 && last >= 0)
+	{
+		if (first == last)
+		{
+			*sum += ai[first];
+			first = last = -1;
+		}
+		else if (ai[first] >= ai[last])
+			*sum += ai[first++];
+		else
+			*sum += ai[last--];
+
+		return 1;
+	}
+
+	return 0;
+}
+
+
 int main(int argc, char *argv[])
 {
-	char s[1000] = { NULL };
+	int n = 0;
 
-	while (scanf("%s", &s) != EOF)
+	while (scanf("%d", &n) != EOF)
 	{
-		printf("hello, %s\n", s);
+		debug(printf("[DEBUG] %d\n", n));
+
+		debug(printf("[DEBUG] "));
+		int i;
+		for (i = 0; i < n; i++)
+		{
+			scanf("%d", &ai[i]);
+			debug(printf("%d ", ai[i]));
+		}
+		debug(printf("\n"); );
+
+		first = 0;
+		last = n - 1;
+		int O = 0, X = 0;
+
+		while (1)
+		{
+			if (!getBigNum(&O))
+				break;
+
+			if (!getBigNum(&X))
+				break;
+		}
+
+		if (O == X)
+			printf("D\n");
+		else
+			printf("%c\n", (O > X) ? 'O' : 'X');
+
+		printf("%d %d\n", O, X);
+
 	}
 
 	return 0;
