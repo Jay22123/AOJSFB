83a6519f0e4349e6b4daa6e2c24710a5ca7bcf3e
PM2011 <abc5593449@hotmail.com>
2016-10-25 17:08:50 +0800

    順時鐘螺旋完成

--- a417螺旋矩陣/main.c
+++ a417螺旋矩陣/main.c
@@ -6,9 +6,53 @@
 
 #include <stdio.h>
 
+void setValue(int mat[100][100], int N, int level, int value)
+{
+	const int ArrSize = (N + 2 * level - 1) - level;
+	int r, c;
+
+	for (c = level; c < ArrSize; c++)
+	{
+		mat[level][c] = value++;
+	}
+
+	for (r = level; r < ArrSize; r++)
+	{
+		mat[r][ArrSize] = value++;
+	}
+
+	for (c = ArrSize; c > level; c--)
+	{
+		mat[ArrSize][c] = value++;
+	}
+
+	for (r = ArrSize; r > level; r--)
+	{
+		mat[r][level] = value++;
+	}
+
+	if (N - 2 == 1)
+		mat[(ArrSize + level) / 2][(ArrSize + level) / 2] = value;
+	else if (N - 2 > 1)
+		setValue(mat, N - 2, level + 1, value);
+}
+
+void printMat(int mat[100][100], int N)
+{
+	int r, c;
+	for (r = 0; r < N; r++)
+	{
+		for (c = 0; c < N; c++)
+		{
+			printf("%5d", mat[r][c]);
+		}
+		printf("\n");
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	int T = 0;
+	int T = 0, mat[100][100] = { 0 };
 
 	while (scanf("%d", &T) != EOF)
 	{
@@ -20,6 +64,9 @@ int main(int argc, char *argv[])
 			int N, M;
 			scanf("%d %d", &N, &M);
 			debug(printf("[DEBUG] %d %d\n", N, M));
+
+			setValue(mat, N, 0, 1);
+			printMat(mat, N);
 		}
 	}
 
